280ec378c697b7632e0cb2fc74feb91663e7323a
Swaryaletsang <atau@student.wethinkcode.co.za>
2019-07-21 00:48:36 +0200

    ls

---
+++ ft_ls.c
@@ -0,0 +1,22 @@
+#include "ft_ls.h"
+#include "libft/libft.h"
+
+void ft_ls()
+{
+    DIR *current_dir;
+    struct dirent *r;
+    
+
+    current_dir = opendir(".");
+    while(r = readdir(current_dir))
+    {
+		if(r->d_name[0] != '.')
+		{
+			ft_putstr(r->d_name);
+			ft_putchar(' ');
+			ft_putchar(' ');
+		}
+
+	}
+    closedir(current_dir);
+}
---
+++ ft_ls.h
@@ -0,0 +1,8 @@
+#ifndef FT_LS_H
+# define FT_LS_H
+#include <sys/types.h>
+#include <dirent.h>
+#include "libft/libft.h"
+
+void        ft_ls();
+#endif
